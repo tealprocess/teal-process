0ed19f1aea09f9f83089647173ef3ca0e1ef6b59
Benji Shankwitz <bshankwitz@gmail.com>
2019-08-13 09:59:45 -0400

    default as gassed.html

--- build.py
+++ build.py
@@ -1,20 +1,26 @@
 import os
 
-default = "gassed"
+default = "gassed.html"
 
 for filename in os.listdir("views"):
 	if filename.endswith(".html"):
+		print filename
 		new_home = os.path.splitext(filename)[0]
 		if not os.path.exists(new_home):
 			os.makedirs(new_home)
 		old_filename = "views/"+filename
-		print(old_filename)
 		with open(old_filename, "r") as orig_file:
 			info = orig_file.read()
 
 			new_filename = new_home+"/index.html"
-			print(new_filename)
 			with open(new_filename, "w") as new_file:
 				new_file.write(info)
+		if filename == default:
+			print default
+			with open(old_filename, "r") as orig_file:
+				info = orig_file.read()
+				new_filename = "index.html"
+				with open(new_filename, "w") as new_file:
+					new_file.write(info)
 	else:
 		continue
